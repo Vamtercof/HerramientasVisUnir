e530d9ed2d47fc39b57c1994b5562d2441cb8a65
unknown <alucard_a1@hotmail.com>
2022-06-22 22:45:54 -0500

    se agrego codigo de ddl y dataset

--- ProyectoD3Equipo/js/app.js
+++ ProyectoD3Equipo/js/app.js
@@ -37,12 +37,12 @@ const g = svg
   .append("g")
   .attr("transform", `translate(${margins.left}, ${margins.top})`)
 
-const load = async () => {
+/*const load = async () => {
   // Carga de Datos
-  data = await d3.csv("Deuda_publica_trimestral.csv", d3.autoType)
+  data = await d3.csv("mercado_inmobiliario.csv", d3.autoType)
   console.log("Data original: ", data);
   
-  // Limpiar el objeto - Mapear { Deuda(y), Periodo(x = [Año y Periodo]) }
+  //Limpiar el objeto - Mapear { Deuda(y), Periodo(x = [Año y Periodo]) }
   data = data.map((i) => {
     let deuda = i.Deuda.replace(/,/g, '');
     
@@ -113,5 +113,110 @@ const load = async () => {
     .call(xAxis)
   const yAxisGroup = g.append("g").classed("axis", true).call(yAxis)
 }
+*/
+const draw = async (variable = "Año") => {
+  // Carga de Datos
+  data = await d3.csv("mercado_inmobiliario.csv", d3.autoType)
 
-load()
+   console.log(data)
+  // console.log(Object.keys(data[0]).slice(1))
+  metrica
+    .selectAll("option")
+    .data(Object.keys(data[0]).slice(1))
+    .enter()
+    .append("option")
+    .attr("value", (d) => d)
+    .text((d) => d)
+
+  // Accessor
+  const xAccessor = (d) => d.Tienda
+
+  // Escaladores
+  const y = d3.scaleLinear().range([alto, 0])
+  const color = d3
+    .scaleOrdinal()
+    .domain(Object.keys(data[0]).slice(1))
+    .range(d3.schemeTableau10)
+
+  // console.log(data)
+  // console.log(d3.map(data, xAccessor))
+
+  const x = d3.scaleBand().range([0, ancho]).paddingOuter(0.2).paddingInner(0.1)
+
+  const titulo = g
+    .append("text")
+    .attr("x", ancho / 2)
+    .attr("y", -15)
+    .classed("titulo", true)
+
+  const etiquetas = g.append("g")
+
+  const xAxisGroup = g
+    .append("g")
+    .attr("transform", `translate(0, ${alto})`)
+    .classed("axis", true)
+  const yAxisGroup = g.append("g").classed("axis", true)
+
+  const render = (variable) => {
+    // Accesores
+    const yAccessor = (d) => d[variable]
+    data.sort((a, b) => yAccessor(b) - yAccessor(a))
+
+    // Escaladores
+    y.domain([0, d3.max(data, yAccessor)])
+    x.domain(d3.map(data, xAccessor))
+
+    // Rectángulos (Elementos)
+    const rect = g.selectAll("rect").data(data, xAccessor)
+    rect
+      .enter()
+      .append("rect")
+      .attr("x", (d) => x(xAccessor(d)))
+      .attr("y", (d) => y(0))
+      .attr("width", x.bandwidth())
+      .attr("height", 0)
+      .attr("fill", "green")
+      .merge(rect)
+      .transition()
+      .duration(2500)
+      // .ease(d3.easeBounce)
+      .attr("x", (d) => x(xAccessor(d)))
+      .attr("y", (d) => y(yAccessor(d)))
+      .attr("width", x.bandwidth())
+      .attr("height", (d) => alto - y(yAccessor(d)))
+      .attr("fill", (d) =>
+        xAccessor(d) == "Satélite" ? "#f00" : color(variable)
+      )
+
+    const et = etiquetas.selectAll("text").data(data)
+    et.enter()
+      .append("text")
+      .attr("x", (d) => x(xAccessor(d)) + x.bandwidth() / 2)
+      .attr("y", (d) => y(0))
+      .merge(et)
+      .transition()
+      .duration(2500)
+      .attr("x", (d) => x(xAccessor(d)) + x.bandwidth() / 2)
+      .attr("y", (d) => y(yAccessor(d)))
+      .text(yAccessor)
+
+    // Títulos
+    titulo.text(`${variable} de las Tiendas`)
+
+    // Ejes
+    const xAxis = d3.axisBottom(x)
+    const yAxis = d3.axisLeft(y).ticks(8)
+    xAxisGroup.transition().duration(2500).call(xAxis)
+    yAxisGroup.transition().duration(2500).call(yAxis)
+  }
+
+  // Eventos
+  metrica.on("change", (e) => {
+    e.preventDefault()
+    // console.log(e.target.value, metrica.node().value)
+    render(e.target.value)
+  })
+  render(variable)
+}
+
+draw()
